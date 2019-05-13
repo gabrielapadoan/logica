e46dd71aa0eed15fcfbf0a8e5d59f91bc92de111
gabrielapadoan <50590788+gabrielapadoan@users.noreply.github.com>
2019-05-13 14:28:40 -0300

    dados 2

--- gabriela/dados.py
+++ gabriela/dados.py
@@ -1,10 +1,29 @@
 #Meu primeiro prograna
-
 from random import randint # IMporta a funao de gerar números aleatórios 
 
-print ("Olá 1INFO2")
+print("Jodos do dado:")
 
-dado1= randint(1,6) # gera um númeroentre entre 1 e 5 aleatóriamente  
-print ("Dado 1:"), dado1
-dado=2=randint(1,6)
-print ("Dado2:")
+dado1 = randint(1,6) # gera um númeroentre entre 1 e 5 aleatóriamente  
+print("Dado 1: ", dado1)
+
+dado2 = randint(1,6)
+print("Dado 2: ", dado2)
+
+dado3 = randint(1,6)
+print("Dado3: ", dado3)
+
+dado4 = randint (1,6)
+print("Dado4: ", dado4)
+
+Jogador1 = dado1 + dado2
+Jogador2 = dado3 + dado4
+
+print("Jogador 1: ", Jogador1)
+print("Jogador 2: ", Jogador2)
+
+if Jogador1 > Jogador2:
+    print("Jogador 1 venceu!")
+elif Jogador2 > Jogador1:
+      print("Jogador 2 venceu!")
+else:
+        print("Empate!")
