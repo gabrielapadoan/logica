59cb857579570d24c29b891b234508897b48a87f
gabrielapadoan <50590788+gabrielapadoan@users.noreply.github.com>
2019-05-13 13:35:50 -0300

    Programa de Dados 1 ° versão

---
+++ gabriela/dados.py
@@ -0,0 +1,10 @@
+#Meu primeiro prograna
+
+from random import randint # IMporta a funao de gerar números aleatórios 
+
+print ("Olá 1INFO2")
+
+dado1= randint(1,6) # gera um númeroentre entre 1 e 5 aleatóriamente  
+print ("Dado 1:"), dado1
+dado=2=randint(1,6)
+print ("Dado2:")
